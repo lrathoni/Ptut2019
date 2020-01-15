82e1c15783fb67d59e3cea35ba5082c45867446c
Ptut <jules.fouchy@ntymail.com>
2020-01-15 15:06:42 +0100

    Add character translation with keyboard

--- MutationsImmobilieresVR/Assets/CharacterControls.cs
+++ MutationsImmobilieresVR/Assets/CharacterControls.cs
@@ -14,7 +14,9 @@ public class CharacterControls : MonoBehaviour
     public SteamVR_Input_Sources handType;
 
     void Update(){
-        Vector2 dl = speed * Time.deltaTime * moveAction.GetAxis(handType);
+        Vector2 dlVR = speed * Time.deltaTime * moveAction.GetAxis(handType);
+        Vector2 dlKeyboard = speed * Time.deltaTime * new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 dl = dlVR + dlKeyboard;
         Vector3 move = cameraTransform.right * dl.x + cameraTransform.forward * dl.y;
         move.y = 0;
         controller.Move(move);
