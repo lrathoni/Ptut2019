2a0c6b53165f675c93b5bcda30f098fb1bece8cf
Line <line.jpn25@gmail.com>
2020-01-15 11:32:41 +0100

    Unity folder VR version

---
+++ MutationsImmobilieresVR/Assets/CharacterControls.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CharacterControls : MonoBehaviour
+{
+    public float speed = 8.0f;
+    public CharacterController controller;
+    public Transform camTransform;
+
+    public SteamVR_Action_Vector2 moveAction;
+    public SteamVR_Input_Sources handType;
+
+
+    void Update(){
+        Vector2 dl = speed * Time.deltaTime * moveAction.GetAxis(handType);
+        Vector3 move = camTransform.right * dl.x + camTransform.forward * dl.y;
+        move.y = 0;
+        controller.Move(move);
+    }
+}
---
+++ MutationsImmobilieresVR/Assets/Room/OpenDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Transform playertransform;
+    int i = 0;
+    bool Open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Get YAxis
+        Vector3 proximity = playertransform.transform.position - transform.position;
+        if ( proximity.magnitude < 5 && proximity.magnitude > 1 && Open == false ) {
+            if ( i > -100) {
+                transform.Rotate(0.0f, -1.0f, 0.0f, Space.World);
+                i-=1;
+            }
+            if (i == -100) 
+            {
+                Open = true;
+            }
+        }
+        if (proximity.magnitude > 5 && Open == true ) {
+            if ( i < 0)
+            {
+                transform.Rotate(0.0f, +2.0f, 0.0f, Space.World);
+                i += 2;
+            }
+            if ( i == 0) 
+            {
+                Open = false;
+            }
+        } 
+    }
+}
