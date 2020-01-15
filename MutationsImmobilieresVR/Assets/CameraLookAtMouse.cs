ea02483540ad454f5b0ec7c66f020eba46def451
Ptut <jules.fouchy@ntymail.com>
2020-01-15 15:18:32 +0100

    Control camera look at  with mouse

---
+++ MutationsImmobilieresVR/Assets/CameraLookAtMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAtMouse : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public Transform camTransform;
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camTransform.Rotate(Vector3.right * xRotation);
+        camTransform.Rotate(Vector3.up * mouseX);
+    }
+}
