d422913eda3ac3c8e5147faf832b209af1d4c9b2
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-16 15:37:47 +0100

    ChairFlight : add some horizontal movement

--- MutationsImmobilieresVR/Assets/Armchair+Wings/ChairFlight.cs
+++ MutationsImmobilieresVR/Assets/Armchair+Wings/ChairFlight.cs
@@ -10,7 +10,9 @@ public class ChairFlight : MonoBehaviour
     public float minDist = 35f;
     public float minFlightHeight = 0.5f;
     public float maxFlightHeight = 1.5f;
-    public float flightPuls = 1.8f;
+    public float maxXZMovementRadius = 2f;
+    public float heightPuls = 0.8f;
+    public float xzPuls = 0.02f;
 
     void Update()
     {
@@ -18,11 +20,17 @@ public class ChairFlight : MonoBehaviour
         Vector2 chairXZ = new Vector2(transform.position.x, transform.position.z);
         float t = Mathf.Max(1f - (playerXZ - chairXZ).magnitude / minDist, 0f);
         t = Mathf.Pow(t, 3f);
+        // Height variation
         float minHeight = t * minFlightHeight;
         float maxHeight = t * maxFlightHeight;
-        float actualHeight = Mathf.Lerp(minHeight, maxHeight, Mathf.PerlinNoise(Time.time * flightPuls, Time.time * flightPuls));
-        Debug.Log(Mathf.PerlinNoise(Time.time * flightPuls, Time.time * flightPuls));
-        transform.localPosition = new Vector3(transform.localPosition.x, actualHeight, transform.localPosition.z);
+        float actualHeight = Mathf.Lerp(minHeight, maxHeight, Mathf.PerlinNoise(Time.time * heightPuls, Time.time * heightPuls));
+        // XZ movement
+        float movRadius = t * maxXZMovementRadius;
+        float angle = Mathf.Lerp(0, Mathf.PI, Mathf.PerlinNoise(Time.time * xzPuls + 50f, Time.time * xzPuls));
+        float actualRadius = Mathf.Lerp(0, movRadius, Mathf.PerlinNoise(Time.time * xzPuls + 100f, Time.time * xzPuls));
+        Vector2 XZpos = actualRadius * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        // Set transform
+        transform.localPosition = new Vector3(XZpos.x, actualHeight, XZpos.y);
         // Wings scale
         float tWings = Mathf.Min(t * 5f, 1f);
         float scl = maxWingsScale * tWings;
