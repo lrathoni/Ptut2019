637bb4239b5dbf63c0f113e29f73856ab604b3d6
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-16 13:51:38 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ MutationsImmobilieresVR/Assets/Armchair+Wings/ChairFlight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairFlight : MonoBehaviour
+{
+    public Transform player;
+    public float minDist = 10f;
+    public float maxFlightHeight = 3f;
+
+    void Update()
+    {
+        float t = Mathf.Max(1f - (player.position - transform.position).magnitude / minDist, 0f);
+        //Debug.Log((player.position - transform.position).magnitude);
+    }
+}
