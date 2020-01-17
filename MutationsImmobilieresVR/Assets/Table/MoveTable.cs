b670c001a6751a485e44ab4340963e197b54fe27
Monica <monicalisacek@hotmail.com>
2020-01-17 16:37:14 +0100

    Table movement v2

--- MutationsImmobilieresVR/Assets/Table/MoveTable.cs
+++ MutationsImmobilieresVR/Assets/Table/MoveTable.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 public class MoveTable : MonoBehaviour
 {
     public Transform player;
+    public Rigidbody rb;
     public Vector3 bottom_left = new Vector3(240, 0, 219);
     public Vector3 bottom_right = new Vector3(203, 0, 219);
     public Vector3 up_left = new Vector3(240, 0, 153);
     public Vector3 up_right = new Vector3(203, 0, 153);
+    
 
     double distanceXZ(Vector3 point1, Vector3 point2)
     {
@@ -20,19 +22,28 @@ public class MoveTable : MonoBehaviour
         return Math.Sqrt(a * a + b * b);
     }
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //transform.position = new Vector3(0,80,0);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //if the player gets closer to the bottom/left, the table will go there
+        //if the player is in a corner of the room, the table will move towards him
         if (distanceXZ(player.position, bottom_left) < 6)
-            transform.position = new Vector3(transform.position.x+0.5f, transform.position.y, transform.position.z+1);
+            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z + 1);
         if (distanceXZ(player.position, bottom_right) < 6)
             transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z + 1);
         if (distanceXZ(player.position, up_left) < 6)
             transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z - 1);
         if (distanceXZ(player.position, up_right) < 6)
             transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z - 1);
-
     }
 }
 
