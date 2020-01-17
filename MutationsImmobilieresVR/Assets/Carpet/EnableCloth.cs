96309287a957bceabf0bffc08b6607a78c00c9b5
Line <line.jpn25@gmail.com>
2020-01-17 17:05:06 +0100

    Carpet fallen
    If we move in the middle of the carpet, the cloth effect is enabled and the carpet falls.

---
+++ MutationsImmobilieresVR/Assets/Carpet/EnableCloth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCloth : MonoBehaviour
+{
+    public Transform playertransform;
+    int waiting = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Cloth>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Cloth>().enabled == false)
+        {
+            Debug.Log("Cloth distance entrée dans le code");
+                Vector3 collision = playertransform.position;
+                Vector3 center = transform.position;
+            Debug.Log("DistanceCloth = " + (center - collision).magnitude);
+            if ((center - collision).magnitude < 3)
+            {
+                 Debug.Log("waiting = " + waiting);
+                 GetComponent<Cloth>().enabled = true;
+            }
+            
+        }
+    }
+}
