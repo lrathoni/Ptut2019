ef7d0d5c9dc85773d3dc00438e93c463f4bfd70b
Line <line.jpn25@gmail.com>
2020-01-22 11:57:03 +0100

    Script Change textures paint

---
+++ MutationsImmobilieresVR/Assets/Multitexturing/swapTexture.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class swapTexture : MonoBehaviour
+{
+
+    public Texture[] textures;
+    public Renderer m_Renderer;
+    public float changeInterval = 0.33F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textures.Length == 0)
+            return;
+
+        int index = Mathf.FloorToInt(Time.time / changeInterval);
+        index = index % textures.Length;
+        m_Renderer.material.mainTexture = textures[index];
+    }
+}
--- MutationsImmobilieresVR/Assets/Room/Former Room/OpenDoor.cs
+++ MutationsImmobilieresVR/Assets/Room/Former Room/OpenDoor.cs
@@ -19,7 +19,7 @@ public class OpenDoor : MonoBehaviour
         Vector3 PlayerEntered = playertransform.transform.position - detection.transform.position;
         if (PlayerIn == false)
         {
-            if (proximity.magnitude < 10 && proximity.magnitude > 1)
+            if (proximity.magnitude < 13 && proximity.magnitude > 1 && OpenActivated == false)
             {
                 OpenActivated = true;
             }
@@ -36,18 +36,18 @@ public class OpenDoor : MonoBehaviour
                 }
             }
         }
-        if (proximity.magnitude > 10)
+        if ( (proximity.magnitude > 20 && OpenActivated == false) || (proximity.magnitude > 10 && PlayerIn == true))
         {
             if ( i < 0)
             {
-                transform.Rotate(0.0f, +2.0f, 0.0f, Space.World);
-                i += 2;
+                transform.Rotate(0.0f, +1.0f, 0.0f, Space.World);
+                i += 1;
             }
         }
         if (PlayerEntered.magnitude < 5)
         {
             PlayerIn = true;
         }
-        Debug.Log("PlayeEntered = " + PlayerEntered.magnitude);
+        //Debug.Log("PlayeEntered = " + PlayerEntered.magnitude);
     }
 }
