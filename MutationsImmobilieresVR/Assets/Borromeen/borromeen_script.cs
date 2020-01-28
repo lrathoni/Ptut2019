63d67a6e84bc285a0c3f2e3c45ea7a6b06efa96a
Monica <monicalisacek@hotmail.com>
2020-01-28 19:04:22 +0100

    borro script "emptied"

--- MutationsImmobilieresVR/Assets/Borromeen/borromeen_script.cs
+++ MutationsImmobilieresVR/Assets/Borromeen/borromeen_script.cs
@@ -15,27 +15,19 @@ public class borromeen_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if the player is far enough the object will increase in size
-        if (player.position.x - transform.position.x < -5) 
-        {
-            x += 0.002f;
-            transform.localScale = new Vector3(x, x, x);
-            if (x > 3) x = 0;
-        }
-        else
-        {
-            x = 0.2f;
-            transform.localScale = new Vector3(x, x, x);
-        }
+		//if the player is far enough the object will increase in size
+		//if (player.position.x - transform.position.x < -5) 
+		//{
+		//    x += 0.002f;
+		//    transform.localScale = new Vector3(x, x, x);
+		//    if (x > 3) x = 0;
+		//}
+		//else
+		//{
+		//    x = 0.2f;
+		//    transform.localScale = new Vector3(x, x, x);
+		//}
 
-        ////movement of object
-        //transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z + 0.5f);
-        //if (transform.position.x < 205 || transform.position.z < 153
-        //    || transform.position.x > 240 || transform.position.z > 219)
-        //{
-        //    transform.position = new Vector3(220, transform.position.y, 200);
-        //}
-
-    }
+	}
 
 }
