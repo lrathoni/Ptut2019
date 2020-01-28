8b18bf88c7fab10c09269614cf6b2518027f2510
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-28 14:43:06 +0100

    Add event : room grows

---
+++ MutationsImmobilieresVR/Assets/ChangeScale.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnEventIDChange(int id)
+    {
+        if (id%3 == 0 && id / 9 == 1)
+        {
+            //transform.localScale = new Vector3(2, 1 , 1);
+            animator.SetBool("Grow", true);
+        }
+        else
+        {
+            //transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("Grow", false);
+        }
+    }
+}
