3755dc4b8d4084cf6a8578678e9f7f8e6a10e4d3
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-29 19:10:36 +0100

    Weird sphere appears and disappears

---
+++ MutationsImmobilieresVR/Assets/Deformable Sphere/SphereScaleAnimationCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SphereScaleAnimationCtrl : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnEventIDChange(int id)
+    {
+        if (id % 4 == 0)
+        {
+            animator.SetBool("Play", true);
+            GetComponent<DeformMesh>().enabled = true;
+        }
+        else
+        {
+            animator.SetBool("Play", false);
+            GetComponent<DeformMesh>().enabled = false;
+        }
+    }
+}
