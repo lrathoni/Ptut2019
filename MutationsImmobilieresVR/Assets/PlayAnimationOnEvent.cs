1ced211b762825500b63fea9bbfb14186b2111df
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-29 18:58:22 +0100

    Rotation of vase in room2

---
+++ MutationsImmobilieresVR/Assets/PlayAnimationOnEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimationOnEvent : MonoBehaviour
+{
+    Animator animator;
+    public int eventPeriod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    void OnEventIDChange(int id)
+    {
+        if (id % eventPeriod == 0)
+        {
+            animator.SetBool("Play", true);
+        }
+        else
+        {
+            animator.SetBool("Play", false);
+        }
+    }
+}
