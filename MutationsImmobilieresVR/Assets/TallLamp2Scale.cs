cf9155eb67b41e8d6d4da21a92aad82c5204abb3
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-30 14:21:19 +0100

    Room2 : tall lamp Templier dance

---
+++ MutationsImmobilieresVR/Assets/TallLamp2Scale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TallLamp2Scale : MonoBehaviour
+{
+    float Frac(float x)
+    {
+        return x - Mathf.Floor(x);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventsManager.I.getEventID() % 6 == 0)
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, 2.54f * Frac(Mathf.Sin(Time.time * 5000f)));
+    }
+}
