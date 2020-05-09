cb9ad710ad32d39a46737d58bfc4fe6cf9b8003a
Line <line.jpn25@gmail.com>
2020-05-09 15:57:49 +0200

    Add musics decalage

---
+++ MutationsImmobilieresVR/Assets/Room/Former Room/RoomtoFlatSound.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomtoFlatSound : MonoBehaviour
+{
+    public Transform player;
+    public Transform RoomMusic;
+    public Transform FlatMusic;
+
+    bool playerIn = false;
+    // Start is called before the first frame update
+    void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 proximity = player.transform.position - transform.position;
+
+            if (proximity.magnitude < 5f && GetComponent<AudioSource>().volume == 0f)
+                playerIn = true;
+
+            if (playerIn == true)
+            {
+                RoomMusic.GetComponent<AudioSource>().enabled = true;
+
+                if (RoomMusic.GetComponent<AudioSource>().volume < 0.8f && playerIn == true)
+                {
+                    RoomMusic.GetComponent<AudioSource>().volume += Time.deltaTime * 0.1f;
+                    FlatMusic.GetComponent<AudioSource>().volume -= Time.deltaTime * 0.3f;
+                }
+
+                if (GetComponent<AudioSource>().volume > 0.8f && playerIn == true)
+                {
+                    FlatMusic.GetComponent<AudioSource>().volume = 0;
+                    playerIn = false;
+                    FlatMusic.GetComponent<AudioSource>().enabled = false;
+                }
+            }
+        }
+    
+}
+
